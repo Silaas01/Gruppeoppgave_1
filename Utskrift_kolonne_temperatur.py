a8d91de64049933c95e1d89e8aaccc4d5f7057b3
Silaas01 <sil.aase@stud.uis.no>
2024-10-06 21:02:52 +0200

    Update Utskrift_kolonne_temperatur.py

--- Utskrift_kolonne_temperatur.py
+++ Utskrift_kolonne_temperatur.py
@@ -2,7 +2,7 @@
 from Kolonne_temperatur import hent_kolonne_temp
 
 # Hent verdiene fra den første scriptet
-hent_kolonne_temp = hent_kolonne_temp()
+kolonne_temp = hent_kolonne_temp()
 
 # Skriv ut listen
 print("Verdiene i kolonne_temp:")
