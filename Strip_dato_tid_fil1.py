aafc6b762065870dda15c675b5a1d2b3f9cd9fe6
lamee-s <240704lm@gmail.com>
2024-10-08 15:30:12 +0200

    Update Strip_dato_tid_fil1.py

--- Strip_dato_tid_fil1.py
+++ Strip_dato_tid_fil1.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from Konvertering_temp_per_min import data_list  # Importerer listen fra konvertert datafil script
 
 # Funksjonen for å trekke ut dato og klokkeslett fra en liste av ordbøker
-def hent_ut_datetime(data_list):
-    datetime_set = set()  # Bruker et sett for å unngå duplikater
+def hent_ut_dato_tid(data_list):
+    dato_tid_set = set()  # Bruker et sett for å unngå duplikater
 
     for entry in data_list:
         dato_tid_str1 = entry["dato_tid"]  # Trekk ut dato- og klokkeslettstrengen fra ordboken
@@ -16,12 +16,14 @@ def hent_ut_datetime(data_list):
             dato_tid_obj1 = datetime.strptime(dato_tid_str1, '%d.%m.%Y %H:%M')  # Formatet "%d.%m.%Y %H:%M"
 
             # Legg til den ekstraherte datoen og klokkeslettet som en streng i settet, nå som DD HH:MM
-            datetime_set.add(dato_tid_obj1.strftime("%d,%H:%M"))  # Format: DD HH:MM
+            dato_tid_set.add(dato_tid_obj1.strftime("%d,%H:%M"))  # Format: DD HH:MM
 
     # Returner en sortert liste med unike datoer og klokkeslett
-    return sorted(list(datetime_set))
+    return sorted(list(dato_tid_set))
 
 # Hent ut dato og klokkeslett fra den importerte listen
-extracted_datetimes = hent_ut_datetime(data_list)
+hentet_datetimes = hent_ut_dato_tid(data_list)
 
-print(extracted_datetimes)
+print("Dato og tid:")
+for datetime_str in hentet_datetimes: #Skriv ut hver dato og klokkeslett fra listen på en egen linje
+    print(datetime_str)
