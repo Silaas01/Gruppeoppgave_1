304b5edf4e9e5684ab44cbd6b220b3a9a788591e
lamee-s <240704lm@gmail.com>
2024-10-09 14:03:42 +0200

    Update Reduser_støy(opp_g).py

--- Reduser_støy(opp_g).py
+++ Reduser_støy(opp_g).py
@@ -1,5 +1,3 @@
-
-
 from Split_konvertert_tid import tid_kolonne  # Import the list of time points from a separate script
 from Split_konverter_temp import temperatur_kolonne  # Import the list of temperature values from another script
 
@@ -7,27 +5,24 @@ from Split_konverter_temp import temperatur_kolonne  # Import the list of temper
 temperaturer = [float(temp.replace(',', '.')) for temp in temperatur_kolonne 
                 if isinstance(temp, (int, float, str)) and str(temp).replace('.', '', 1).isdigit()]
 
+# Sikrer at hver tid konverteres til en gyldig type.
+tidspunkt = [tid for tid in tid_kolonne if isinstance(tid, (str, int, float))]
+
 def beregn_gjennomsnitt(tidspunkt, temperaturer, n):
     gyldige_tidspunkt = []  # Tom liste for å lagre gyldige tidspunkter
     gjennomsnittelige_verdier = []  # Tom liste for å lagre gjennomsnittlige temperaturverdier
 
     # Gå gjennom gyldige utvalgte indekser for gjennomsnittsberegning
     for i in range(n, len(temperaturer) - n):
-       # 'i' varierer fra n til "len(temperaturer) - n" for å sikre at vi ikke går utenfor grensene.
+        # 'i' varierer fra n til "len(temperaturer) - n" for å sikre at vi ikke går utenfor grensene.
         
         maalinger = temperaturer[i-n:i+n+1]  # Slicing:temperature listen for å få de n forrige,nåværende, og de n neste målingene
 
         gjenn = sum(maalinger) / len(maalinger)  # Beregn gjennomsnittet av målingene
 
         # Legg til det tilsvarende gyldige tidsstempelet og det beregnede gjennomsnittete
-        gyldige_tidspunkt.append(tidspunkt[i])  # Lagre tilsvarende tidspunktet fra 'tid_kolone'
+        gyldige_tidspunkt.append(tidspunkt[i])  # Lagre tilsvarende tidspunktet fra 'tid_kolonne'
         gjennomsnittelige_verdier.append(gjenn)  # Lagre gjennomsnittstemperatur verdien 
 
     return gyldige_tidspunkt, gjennomsnittelige_verdier
     # Returner listene over gyldige tidspunkter og gjennomsnittstemperatur verdier
-
-#Fjern eksempelbruksdelen fra scriptet når du plotter
-
-n = 2  
-gyldige_tid, gjennomsnitt = beregn_gjennomsnitt(tid_kolonne, temperaturer, n)  
-
